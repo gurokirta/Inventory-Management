56c6600efb4a0185af2c07c129707d54412f965d
Guro <Guro@github.com>
2024-10-04 00:26:21 +0400

    commit from backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,7 @@ app.use(cors());
 app.use("/dashboard", dashboardRoutes);
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

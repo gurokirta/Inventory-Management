de4c5e652a73c2fd86453f439f55178636bf60ae
Guro <Guro@github.com>
2024-10-03 20:13:35 +0400

    lounched server

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,5 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // ROUTES
+app.get("/hello", (req, res) => {
+  res.send("Hello World");
+});
 
 // Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

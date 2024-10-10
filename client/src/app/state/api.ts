9655e8603427635f2be71d2a8fb06be4a80ad978
Guro <Guro@github.com>
2024-10-10 23:14:33 +0400

    created inventory page

--- client/src/app/state/api.ts
+++ client/src/app/state/api.ts
@@ -8,6 +8,13 @@ export interface Product {
   stockQuantity: number;
 }
 
+export interface NewProduct {
+  name: string;
+  price: number;
+  rating?: number;
+  stockQuantity: number;
+}
+
 export interface SalesSummary {
   salesSummaryId: string;
   totalValue: number;
@@ -46,13 +53,29 @@ export interface DashboardMetrics {
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: ["DashboardMetrics"],
+  tagTypes: ["DashboardMetrics", "Product"],
   endpoints: build => ({
     getDashboardMetrics: build.query<DashboardMetrics, void>({
       query: () => "/dashboard",
       providesTags: ["DashboardMetrics"],
     }),
+    getProducts: build.query<Product[], string | void>({
+      query: search => ({ url: "/products", params: search ? { search } : {} }),
+      providesTags: ["Product"],
+    }),
+    createProduct: build.mutation<Product, NewProduct>({
+      query: newProduct => ({
+        url: "/products",
+        method: "POST",
+        body: newProduct,
+      }),
+      invalidatesTags: ["Product"],
+    }),
   }),
 });
 
-export const { useGetDashboardMetricsQuery } = api;
+export const {
+  useGetDashboardMetricsQuery,
+  useCreateProductMutation,
+  useGetProductsQuery,
+} = api;
